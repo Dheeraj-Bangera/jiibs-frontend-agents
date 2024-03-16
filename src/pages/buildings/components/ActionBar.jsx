e3877fa9cd04f4234fd6eed807dde6488b4db402
roshgupta <gpta.duke.rohan.1219@gmail.com>
2024-03-16 21:03:03 +0530

    fixed the issue

--- src/pages/buildings/components/ActionBar.jsx
+++ src/pages/buildings/components/ActionBar.jsx
@@ -2,36 +2,47 @@ import React from "react";
 import Button from "../../../components/Button/Button";
 import left_arrow from "../../../assets/left_arrow.png";
 import right_arrow from "../../../assets/right_arrow.png";
-import axios from "axios"
+import axios from "axios";
 
 export default function ActionBar({ title, formData }) {
-  const postData =async ()=>{
-    console.log(formData);
+  // fix the problems in my code
+  const postData = async () => {
     const newFormData = new FormData();
     for (let key in formData) {
-      if (typeof formData[key] === "object" && formData[key] !== null) {
-          newFormData.append(key, JSON.stringify(formData[key]));
-          console.log("inside");
-      } else {
-          newFormData.append(key, formData[key]);
-          
-          console.log("inside");
-      }
-  }
-  console.log(newFormData);
-    const res = await axios.post("http://localhost:8080/api/building/addBuilding",newFormData)
-    alert(res.data.message)
-  }
+      // if (typeof formData[key] === "object" && formData[key] !== null) {
+      newFormData.append(key, JSON.stringify(formData[key]));
+      // } else {
+      // newFormData.append(key, formData[key]);
+      // }
+    }
+    for (let key in formData) {
+      console.log(`${key} : ${newFormData.get(key)}`);
+    }
+    try {
+      const res = await axios.post(
+        "http://localhost:8080/api/building/addBuilding",
+        newFormData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      alert(res.data.message);
+    } catch (error) {
+      console.error("Error uploading data:", error);
+    }
+  };
   return (
     <div className="flex items-center w-full justify-between max-sm:justify-end max-sm:pr-[15px]">
       <div className="text-[16px] sm:text-[26px] font-semibold max-sm:hidden sm:leading-[80%]">
         {title}
       </div>
       <div className="flex gap-x-4">
-        <Button style="secondary" onClick={postData} >Publish</Button>
-        <Button onClick={() => console.log(formData)} style="secondary">
-          View
+        <Button style="secondary" onClick={postData}>
+          Publish
         </Button>
+        <Button style="secondary">View</Button>
         <div className="">
           <button
             className={`bg-button-secondary text-primar px-3 rounded-tl rounded-bl text-[14px] h-[32.6px]`}
